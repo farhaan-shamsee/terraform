744101afc317248ddc424ba97d0d78ff2062ee23
Farhaan <farhaan39@gmail.com>
2023-01-04 15:21:19 +0530

    adding git ignore

---
+++ 5.github_example/repositories.tf
@@ -0,0 +1,9 @@
+resource "github_repository" "terraform_test_repo" {
+  name        = "terraform_test_repo"
+  description = "My test repo created using Terraform"
+
+  visibility = "public"
+
+  auto_init = true  
+  #this creates a README.md file automatically
+}
