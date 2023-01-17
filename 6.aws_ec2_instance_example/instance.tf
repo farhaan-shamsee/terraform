3f7fb72ca0832ec3fe90ff64563082bee8b58a46
farhaan shamsee <farhaan.shamsee@siemens.com>
2023-01-17 09:14:24 +0530

    modified file provisioners

--- 6.aws_ec2_instance_example/instance.tf
+++ 6.aws_ec2_instance_example/instance.tf
@@ -8,17 +8,21 @@ resource "aws_instance" "web" {
   security_groups = ["${aws_security_group.allow_tls.name}"] # Referring to the SG getting created above.
   user_data       = file("${path.module}/script.sh")
 
-# 3 provisioners: file, local-exec, remote-exec
-  provisioner "file" {
-    source = "readme.md" #terraform machine
-    destination = "/tmp/readme.md" #remote machine
-    connection {
-      type = "ssh"
-      user = "ubuntu"
-      private_key = file("${path.module}/id_rsa")
+  # 3 provisioners: file, local-exec, remote-exec
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/id_rsa")
     # host = "${aws_instance.web.public_ip}" 
-      # this will create a deadlock condition as the instance is dependent on tis own IP address
-      host = "${self.public_ip}"
-    }
+    # this will create a deadlock condition as the instance is dependent on tis own IP address
+    host = self.public_ip
+  }
+  provisioner "file" {
+    source      = "readme.md"      #terraform machine
+    destination = "/tmp/readme.md" #remote machine
+  }
+  provisioner "file" {
+    content     = "this is copied to the file at destination" #terraform machine
+    destination = "/tmp/content.md"                           #remote machine
   }
 }
